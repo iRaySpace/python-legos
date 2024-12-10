d2863976febb001c40d01c6c06ed1a46d6bd5617
irayspace <irayspacii@gmail.com>
2024-12-10 15:26:05 +0800

    feat: mirror

--- video-extender/app/__main__.py
+++ video-extender/app/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 from math import ceil
 from moviepy import VideoFileClip
+# from moviepy.video.fx.MirrorX import MirrorX
 from moviepy.video.compositing.CompositeVideoClip import concatenate_videoclips
 
 
@@ -16,9 +17,12 @@ def _get_args():
 def main():
     args = _get_args()
     clip = VideoFileClip(f'./uploads/{args.filename}')
+    # clip = clip.with_effects([MirrorX()])
+    # clip.write_videofile(f'./results/flipped_{args.filename}')
     number_of_clips = ceil(HOUR_IN_SECS / clip.duration)
     final = concatenate_videoclips([clip for _ in range(number_of_clips)])
     final.write_videofile(f'./results/{args.filename}', fps=30)
+
 
 
 if __name__ == '__main__':
