f419a73a693fccef24d3e234ebd952c44a8a7ccb
irayspace <irayspacii@gmail.com>
2024-12-11 16:53:05 +0800

    feat: 2

--- aoc-2024/2.py
+++ aoc-2024/2.py
@@ -1,14 +1,34 @@
+
+
 def is_safe(numbers):
-    current_number = next(iter(numbers))
+    polarity = None
+
+    for i in range(len(numbers) - 1):
+        window_sum = numbers[i] - numbers[i + 1]
+
+        if window_sum == 0 or abs(window_sum) > 3:
+            return False
+
+        if window_sum > 0:
+            if polarity == -1: return False
+            polarity = 1
+        else:
+            if polarity == 1: return False
+            polarity = -1
+
+    return True
 
 
 def main():
+    safe_numbers = 0
     numbers = []
     with open('./res/2', 'r') as file:
         for line in file:
             line = line.strip()
             numbers = [int(x) for x in line.split(' ')]
-            print(is_safe(numbers))
+            if is_safe(numbers):
+                safe_numbers = safe_numbers + 1
+    print(safe_numbers)
 
 
 if __name__ == '__main__':
